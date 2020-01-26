d4c2b0fd8568381200737227af56a0852f68758d
Billie Devolder <devolder.billie96@gmail.com>
2020-01-26 16:26:24 +0100

    Make tabs clickable

--- src/component/Details.vue
+++ src/component/Details.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <div class="card" style="height: 100%">
-            <div class="card-header">
-                <ul class="nav nav-pills card-header-pills">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">General</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Selection</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body">
+    <div class="card" style="height: 100%">
+        <div class="card-header">
+            <ul class="nav nav-pills card-header-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="pill" href="#general">General</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="pill" href="#details">Selection</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content card-body">
+            <div class="tab-pane container active" id="general">
                 <GeneralDetails :model="model"/>
+            </div>
+
+            <div class="tab-pane container" id="details">
+                <h5 class="text-center">Storage cell</h5>
             </div>
         </div>
     </div>
